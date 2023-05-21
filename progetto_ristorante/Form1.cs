cc3c5ec8d595e1db6c2a1e72248c221540d9c346
DANIELE BERARDINELLI <s24169@iisve.it>
2023-05-21 23:32:42 +0200

    Creata l'interfaccia login

--- progetto_ristorante/Form1.cs
+++ progetto_ristorante/Form1.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 namespace progetto_ristorante
 {
 	public partial class Form1 : Form
@@ -25,6 +27,32 @@ namespace progetto_ristorante
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            string clientId = "Il_tuo_client_id";
+            string redirectUri = "https://localhost"; // L'URL di reindirizzamento registrato nel tuo progetto Google API Console
+
+            string authorizationEndpoint = "https://accounts.google.com/o/oauth2/v2/auth";
+            string tokenEndpoint = "https://oauth2.googleapis.com/token";
+            string userInfoEndpoint = "https://www.googleapis.com/oauth2/v3/userinfo";
+
+            string scope = "openid email profile"; // Scopes richiesti per l'autorizzazione
+
+            // Genera lo stato casuale per l'autenticazione
+            string state = Guid.NewGuid().ToString();
+
+            // Crea l'URL di autorizzazione per reindirizzare l'utente a Google
+            string authorizationUrl = $"{authorizationEndpoint}?response_type=code&client_id={clientId}&redirect_uri={redirectUri}&scope={scope}&state={state}";
+
+            // Apre il browser predefinito dell'utente con l'URL di autorizzazione
+            Process.Start(authorizationUrl);
         }
     }
 }
